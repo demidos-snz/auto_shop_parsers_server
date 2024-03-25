30c0bc6b2c363e4d60555474c6bfa04dc0494163
demidos <sasha-demidkin@ya.ru>
2024-03-25 09:05:38 -0300

    update Dockerfile, .dockerignore, settings.py.default; refactor chrome.py; delete unused files

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 ARG BASE_IMAGE=python:3.10-slim-bullseye
-
 FROM ${BASE_IMAGE}
 
 ENV PYTHONWARNINGS=ignore \
@@ -7,17 +6,19 @@ ENV PYTHONWARNINGS=ignore \
     APP_DIR=/app
 
 WORKDIR $APP_DIR
+RUN useradd -ms /bin/bash $USER_APP
 
 RUN apt-get update && apt-get upgrade -y
 
-RUN useradd -ms /bin/bash $USER_APP
-
-COPY requirements.txt ./
-COPY chrome_114_amd64.deb ./
-
-RUN apt-get install ./chrome_114_amd64.deb -y && pip install -r requirements.txt
+# 2024-3-25
+# Chromium 120.0.6099.224 built on Debian 11.8, running on Debian 11.9
+# ChromeDriver 120.0.6099.224 (3587067cafd6f5b1e567380acb485d96e623ef39-refs/branch-heads/6099@{#1761})
+RUN apt-get install chromium -y
+RUN apt-get install chromium-driver -y
 
 COPY ./ ./
 
+RUN pip install -r requirements.txt
+
 RUN chown -R ${USER_APP}. ${APP_DIR}
-USER ${USER_APP}
+RUN su ${USER_APP}
--- base/drivers/chrome.py
+++ base/drivers/chrome.py
@@ -4,19 +4,36 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 
 
 class ChromeDriver(WebDriver):
-    def __init__(self, settings: dict[str, bool], options: Options = None, service: Service = None, keep_alive=True):
-        if settings.get('headless'):
-            options: Options = Options() if options is None else options
-            options.add_argument('--headless')
-            options.add_argument('--no-sandbox')
-            options.add_argument('--remote-debugging-port=9222')
-            options.add_argument('--disable-extensions')
-            options.add_argument('--disable-gpu')
-            options.add_argument('--disable-dev-shm-usage')
+    # fixme /usr/bin/chromedriver
+    def __init__(
+            self,
+            settings: dict[str, bool],
+            options: Options = None,
+            service: Service = None,
+            keep_alive=True,
+    ):
+        options: Options = self.default_options() if options is None else options
 
-            service: Service = Service(executable_path='./chromedriver')
+        if settings.get('headless'):
+            options.add_argument('--headless')
+
+        service: Service = self.default_service() if service is None else service
 
         super().__init__(options=options, service=service, keep_alive=keep_alive)
+
+    @staticmethod
+    def default_options() -> Options:
+        options: Options = Options()
+        options.add_argument('--no-sandbox')
+        options.add_argument('--remote-debugging-port=9222')
+        options.add_argument('--disable-extensions')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--disable-dev-shm-usage')
+        return options
+
+    @staticmethod
+    def default_service() -> Service:
+        return Service(executable_path='/usr/bin/chromedriver')
 
     def __delete__(self, instance):
         self.close()
